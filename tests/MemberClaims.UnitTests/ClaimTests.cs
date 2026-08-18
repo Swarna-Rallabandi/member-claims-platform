c9a7a7a97eb80343a941423ba635e1e0376a926b
Swarna Rallabandi <swarna.rallabandi@gmail.com>
2026-08-19 01:08:41 +1000

    Demonstrate failing unit test

---
+++ tests/MemberClaims.UnitTests/ClaimTests.cs
@@ -0,0 +1,20 @@
+﻿using MemberClaims.Api.Models;
+
+namespace MemberClaims.UnitTests;
+
+public class ClaimTests
+{
+    [Fact]
+    public void Claim_WithValidAmount_ShouldHavePositiveAmount()
+    {
+        var claim = new Claim
+        {
+            ClaimId = "CLM-20001",
+            MemberId = "MEM-10001",
+            ClaimType = "Dental",
+            Amount = -500.00m
+        };
+
+        Assert.True(claim.Amount > 0);
+    }
+}
